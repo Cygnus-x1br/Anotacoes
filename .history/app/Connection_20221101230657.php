5f9bbbe70cc8990460293ace546f9ae1676facb6
Cygnus-x1br <jeanmarcel83@gmail.com>
2022-11-01 23:08:11 -0300

    Alteração do DB para o servidor remoto

--- .history/app/Connection_20221101230657.php
+++ .history/app/Connection_20221101230657.php
@@ -11,7 +11,7 @@ class Connection
             // $conn = new \PDO("mysql:host=192.168.0.183;dbname=personal_notes;charset=utf8", "root", "aczf0704");
             // $conn = new \PDO("mysql:host=192.168.0.169;dbname=personal_notes;charset=utf8", "root", "aczf0704");
             // $conn = new \PDO("mysql:host=189.55.192.184;dbname=personal_notes;charset=utf8", "jean", "@Czf0704");
-            $conn = new \PDO("mysql:host=jmarc.com.br;dbname=jean_notes;charset=utf8", "jean_jean", "@Czf0704");
+            $conn = new \PDO("mysql:host=162.240.42.110;dbname=jean_notes;charset=utf8", "jean", "@Czf0704");
             //echo 'Connected...';
             return $conn;
         } catch (\PDOException $err) {
